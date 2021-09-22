a688bc481caaef759f8c42aa1343f0ec017c23e1
sukrita257 <64889034+sukrita257@users.noreply.github.com>
2021-09-23 00:49:49 +0530

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,129 @@
+function myfunc() {
+	b1 = document.getElementById("b1").value;
+	b2 = document.getElementById("b2").value;
+	b3 = document.getElementById("b3").value;
+	b4 = document.getElementById("b4").value;
+	b5 = document.getElementById("b5").value;
+	b6 = document.getElementById("b6").value;
+	b7 = document.getElementById("b7").value;
+	b8 = document.getElementById("b8").value;
+	b9 = document.getElementById("b9").value;
+}
+
+flag = 1;
+function mark1() {
+	if (flag == 1) {
+		document.getElementById("b1").value = "X";
+		document.getElementById("b1").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b1").value = "O";
+		document.getElementById("b1").disabled = true;
+		flag = 1;
+	}
+}
+
+function mark2() {
+	if (flag == 1) {
+		document.getElementById("b2").value = "X";
+		document.getElementById("b2").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b2").value = "O";
+		document.getElementById("b2").disabled = true;
+		flag = 1;
+	}
+}
+
+function mark3() {
+	if (flag == 1) {
+		document.getElementById("b3").value = "X";
+		document.getElementById("b3").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b3").value = "O";
+		document.getElementById("b3").disabled = true;
+		flag = 1;
+	}
+}
+
+function mark4() {
+	if (flag == 1) {
+		document.getElementById("b4").value = "X";
+		document.getElementById("b4").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b4").value = "O";
+		document.getElementById("b4").disabled = true;
+		flag = 1;
+	}
+}
+
+function mark5() {
+	if (flag == 1) {
+		document.getElementById("b5").value = "X";
+		document.getElementById("b5").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b5").value = "O";
+		document.getElementById("b5").disabled = true;
+		flag = 1;
+	}
+}
+
+function mark6() {
+	if (flag == 1) {
+		document.getElementById("b6").value = "X";
+		document.getElementById("b6").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b6").value = "O";
+		document.getElementById("b6").disabled = true;
+		flag = 1;
+	}
+}
+
+function mark7() {
+	if (flag == 1) {
+		document.getElementById("b7").value = "X";
+		document.getElementById("b7").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b7").value = "O";
+		document.getElementById("b7").disabled = true;
+		flag = 1;
+	}
+}
+
+function mark8() {
+	if (flag == 1) {
+		document.getElementById("b8").value = "X";
+		document.getElementById("b8").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b8").value = "O";
+		document.getElementById("b8").disabled = true;
+		flag = 1;
+	}
+}
+
+function mark9() {
+	if (flag == 1) {
+		document.getElementById("b9").value = "X";
+		document.getElementById("b9").disabled = true;
+		flag = 0;
+	}
+	else {
+		document.getElementById("b9").value = "O";
+		document.getElementById("b9").disabled = true;
+		flag = 1;
+	}
+}
